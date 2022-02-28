a949e23bd1506ed5e6d91e67d8bc2a42d1570fcf
AriaieBOY <smr.seddighy@gmail.com>
2022-02-28 20:29:56 +0330

    add possible errors

--- src/CaproverSdk.php
+++ src/CaproverSdk.php
@@ -10,6 +10,22 @@ use GuzzleHttp\Client;
 
 class CaproverSdk
 {
+    //Api Possible Errors
+    const OKAY = 100;
+    const OKAY_BUILD_START = 101;
+    const STATUS_ERROR_GENERIC = 1000;
+    const STATUS_ERROR_CAPTAIN_NOT_INITIALIZED = 1001;
+    const STATUS_ERROR_USER_NOT_INITIALIZED = 1101;
+    const STATUS_ERROR_NOT_AUTHORIZED = 1102;
+    const STATUS_ERROR_ALREADY_EXIST = 1103;
+    const STATUS_ERROR_BAD_NAME = 1104;
+    const STATUS_WRONG_PASSWORD = 1105;
+    const STATUS_AUTH_TOKEN_INVALID = 1106;
+    const VERIFICATION_FAILED = 1107;
+
+    const UNKNOWN_ERROR = 1999;
+
+
     protected ?string $token = null;
     protected Client $client;
 
