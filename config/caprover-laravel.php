3353587deea88ec407ac0a1058dfabc76be175ef
AriaieBOY <smr.seddighy@gmail.com>
2022-02-28 22:53:24 +0330

    get ready for first release

--- config/caprover-laravel.php
+++ config/caprover-laravel.php
@@ -1,5 +1,7 @@
 <?php
 // config for Ariaieboy/CaproverLaravel
 return [
-
+    'server'=>env('CAPROVER_SERVER'),
+    'password'=>env('CAPROVER_PASSWORD'),
+    'timeout'=>env('CAPROVER_TIMEOUT',60)
 ];
--- src/CaproverLaravel.php
+++ src/CaproverLaravel.php
@@ -2,6 +2,15 @@
 
 namespace Ariaieboy\CaproverLaravel;
 
-class CaproverLaravel
+use Ariaieboy\CaproverSdk\CaproverSdk;
+
+class CaproverLaravel extends CaproverSdk
 {
+    public function __construct(string $server = null, string $password = null, int $timeout = null)
+    {
+        $server ??= config('caprover-laravel.server') ?? '';
+        $password ??= config('caprover-laravel.password') ?? '';
+        $timeout ??= config('caprover-laravel.timeout') ?? 60;
+        Parent::__construct($server, $password, $timeout);
+    }
 }
