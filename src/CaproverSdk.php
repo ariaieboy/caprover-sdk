ed7b352e75548766c2756e39790322768c33992a
AriaieBOY <smr.seddighy@gmail.com>
2022-02-28 21:35:46 +0330

    add attachNewCustomDomainToApp method to the sdk

--- src/CaproverSdk.php
+++ src/CaproverSdk.php
@@ -5,24 +5,25 @@ namespace Ariaieboy\CaproverSdk;
 
 use Ariaieboy\CaproverSdk\Exceptions\CannotGetTokenException;
 use Ariaieboy\CaproverSdk\Exceptions\CaproverApiCallErrorException;
+use Ariaieboy\CaproverSdk\Exceptions\CaproverErrorException;
 use Ariaieboy\CaproverSdk\Exceptions\InvalidCaproverServerInfoException;
 use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 
 class CaproverSdk
 {
     protected ?string $token = null;
     protected Client $client;
-
     /**
      * @throws \GuzzleHttp\Exception\GuzzleException
      * @throws InvalidCaproverServerInfoException
      */
     public function __construct(protected string $server, protected string $password)
     {
-        $this->server = rtrim($this->server, '/') . '/api/v2';
+        $this->server = rtrim($this->server, '/') . '/api/v2/';
         $this->getAuthToken();
-        $this->client = new Client(['base_url' => $this->server, 'verify' => true, 'headers' => [
+        $this->client = new Client(['base_uri' => $this->server, 'verify' => true, 'headers' => [
             'x-namespace' => 'captain', 'x-captain-auth' => $this->token
         ]]);
     }
@@ -34,20 +35,39 @@ class CaproverSdk
      */
     protected function getAuthToken(): void
     {
-        $client = new Client(['verify' => true]);
+        $client = new Client(['base_uri'=>$this->server,'verify' => true]);
         try {
-            $response = $client->post($this->server . '/login', ['json' => ['password' => $this->password],
+            $response = $client->post( 'login', ['json' => ['password' => $this->password],
                 'headers' => ['x-namespace' => 'captain']]);
             $data = json_decode($response->getBody()->getContents(), false, 512, JSON_THROW_ON_ERROR);
             if ((int)$data->status !== CaproverApiStatus::OKAY) {
                 throw new CaproverApiCallErrorException($data->description ?? '');
             }
             $this->token = $data->data->token;
-
         } catch (Exception $e) {
             throw new InvalidCaproverServerInfoException($e->getMessage());
         }
     }
 
-
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws CaproverApiCallErrorException
+     */
+    public function attachNewCustomDomainToApp(string $appName, string $customDomain): bool
+    {
+        $clearedCustomDomain = parse_url($customDomain)['host'];
+        try {
+            $response = $this->client->post('user/apps/appDefinitions/customdomain', ['json' => [
+                'appName' => $appName,
+                'customDomain' => $clearedCustomDomain
+            ]]);
+            $data = json_decode($response->getBody()->getContents(), false, 512, JSON_THROW_ON_ERROR);
+            if ($data->status === 100) {
+                return true;
+            }
+            throw new CaproverErrorException(message : $data->description??'',code:$data->status);
+        } catch (GuzzleException $e) {
+            throw new CaproverApiCallErrorException($e->getMessage());
+        }
+    }
 }
---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+test('example', function () {
+    $cs = new Ariaieboy\CaproverSdk\CaproverSdk(server: $_SERVER['CAPROVER_SERVER'], password: $_SERVER['CAPROVER_PASSWORD']);
+    try {
+        $cs->attachNewCustomDomainToApp('test','https://test.dir.bz');
+    }catch(\Ariaieboy\CaproverSdk\Exceptions\CaproverErrorException $e) {
+        var_dump($e->getCode());
+        die();
+    }
+});
