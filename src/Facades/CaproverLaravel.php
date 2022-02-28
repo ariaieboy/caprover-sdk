f28bd402764bd9c916b024b884c27942e0af6d12
AriaieBOY <smr.seddighy@gmail.com>
2022-03-01 01:43:09 +0330

    add ide helper doc block for facade

--- src/Facades/CaproverLaravel.php
+++ src/Facades/CaproverLaravel.php
@@ -5,7 +5,12 @@ namespace Ariaieboy\CaproverLaravel\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @see \Ariaieboy\CaproverLaravel\CaproverLaravel
+ * @see \Ariaieboy\CaproverSdk\CaproverSdk::attachNewCustomDomainToApp
+ * @method static bool attachNewCustomDomainToApp(string $appName, string $customDomain)
+ * @see \Ariaieboy\CaproverSdk\CaproverSdk::removeCustomDomain
+ * @method static bool removeCustomDomain(string $appName, string $customDomain)
+ * @see \Ariaieboy\CaproverSdk\CaproverSdk::enableSslForCustomDomain
+ * @method static bool enableSslForCustomDomain(string $appName, string $customDomain)
  */
 class CaproverLaravel extends Facade
 {
