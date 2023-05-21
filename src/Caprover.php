0e8f80385f6905307f65ffed42cbb5128d951bef
ariaieboy <ariaieboy.ir@gmail.com>
2023-05-22 01:58:05 +0330

    add basic headers and configs

--- src/Caprover.php
+++ src/Caprover.php
@@ -6,11 +6,26 @@ use Saloon\Http\Connector;
 
 class Caprover extends Connector
 {
-    public function __construct(readonly protected string $server, readonly protected string $password, protected int $timeout)
+    public function __construct(readonly protected string $server, readonly protected string $password, protected int $timeout = 60)
     {
     }
+
     public function resolveBaseUrl(): string
     {
         return rtrim($this->server, '/') . '/api/v2';
+    }
+
+    protected function defaultHeaders(): array
+    {
+        return [
+            'x-namespace' => 'captain'
+        ];
+    }
+
+    protected function defaultConfig(): array
+    {
+        return [
+            'timeout' => $this->timeout
+        ];
     }
 }
